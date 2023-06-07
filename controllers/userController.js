e3d4323b40f1cfc24e8081c30d73138611ccff35
Abstynent <lukjur94@gmail.com>
2023-06-07 23:14:23 +0100

    populate thoughts and friends when getting single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,9 @@ module.exports = {
     // get single user by ID
     async getSingleUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params.userId});
+            const user = await User.findOne({ _id: req.params.userId})
+            .populate('thoughts')
+            .populate('friends');
 
             if(!user) {
                 return res.status(404).json({ message: 'No user with that ID' });
@@ -25,13 +27,12 @@ module.exports = {
             console.log(error);
             return res.status(500).json(error);
         }
-
     },
     // function to create user
     async createUser(req, res) {
         try {
-            const dbUserData = await User.create(req.body);
-            res.json(dbUserData);
+            const user = await User.create(req.body);
+            res.json(user);
         } catch (error) {
             console.log(error);
             return res.status(500).json(error);
