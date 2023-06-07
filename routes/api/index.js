87d6f8532ca9b3e16035e10e45cada4f80ba521d
Abstynent <lukjur94@gmail.com>
2023-06-07 23:14:50 +0100

    functions to handle routes to get all/create one thought/s

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRotues = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRotues);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
